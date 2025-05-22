b6b2eab8d8a5368468740d59c1729d232f8db37d
egiiputra <egiputra450@gmail.com>
2025-05-22 20:32:20 +0700

    start react project

--- api/index.js
+++ api/index.js
@@ -1,31 +1,9 @@
 import * as cheerio from 'cheerio'
 
+const $ = await cheerio.fromURL(`https://pta.trunojoyo.ac.id/c_search/byprod/10/1`)
 
-const facultiesToNumber = {
-fh: 1,
-fp: 2,
-feb: 3,
-ft: 4,
-fisib: 5,
-fkis: 6,
-fkip: 7,
-}
-
-const faculty = 'fh'
-const $ = await cheerio.fromURL(`https://pta.trunojoyo.ac.id/c_search/byfac/${facultiesToNumber[faculty]}`);
-
-// console.log($)
-// exit()
 const listItems = $('ul.items').children('li')
 
-listItems.each((index, item) => {
-    if (index == 0) {
-        console.log($(item).find('a.title').text())
-        const authors = $(item).find('div > span').map((i, el) => {
-            return $(el).text().split(/\s*:\s*/)[1]
-        }).toArray()
-        console.log(authors[0])
-        console.log(typeof authors)
-        console.log($(item).find('a.button').prop('href'))
-    }
-})
+for (let j = 0; j < listItems.length; j++) {
+    console.log(listItems[j])
+}
