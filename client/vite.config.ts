ade32f725c02114675afa6280b337ba9f7939c70
egiiputra <egiputra450@gmail.com>
2025-05-23 10:36:53 +0700

    setup proxy for api

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,27 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+export default ({ mode }: {mode: string}) =>{
+  
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_HOST,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  })
+}
